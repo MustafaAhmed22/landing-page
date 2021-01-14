6caaee05bcdf75fab96c106f1c136f6728fe807c
Mustafa ahmed <66968111+MustafaAhmed22@users.noreply.github.com>
2021-01-14 17:07:20 +0200

    add remove class

--- js/app.js
+++ js/app.js
@@ -17,8 +17,8 @@
  * Define Global Variables
  * 
 */
-
-
+const navigationBar = document.querySelector('#navbar__list')
+const sections = document.querySelectorAll('section')
 /**
  * End Global Variables
  * Start Helper Functions
@@ -35,13 +35,41 @@
 
 // build the nav
 
+let navSections ='';
+    sections.forEach(section =>{
+        
+        const sectionNav_set = section.dataset.nav;
+        navSections += `<li><a class ='menu__link' href="#${section.id}">
+        ${sectionNav_set}</a></li>`;
+    })
+    navigationBar.innerHTML =navSections;
+
+
 
 // Add class 'active' to section when near top of viewport
-
-
+const offset = section =>{
+    return Math.floor(section.getBoundingClientRect().top)
+}
 // Scroll to anchor ID using scrollTO event
+const remove = section =>{
+    section.classList.remove('your-active-class')
+}
+const add = (addClass ,section) =>{
+    if(addClass){
+        section.classList.add('your-active-class')
 
+    }
+}
 
+const sectionActive = ()=>{
+sections.forEach(section=>{
+    const offsetElement = offset(section)
+    port = ()=>offsetElement < 150 && offsetElement >= -150 ;
+    remove(section)
+    add(port() ,section)
+})
+}
+window.addEventListener('scroll',sectionActive)
 /**
  * End Main Functions
  * Begin Events
